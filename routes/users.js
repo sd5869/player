ce773ea2704c0c286ee209b5254ee4635a7d456d
Siddharth Dungarwal <sd5869@gmail.com>
2017-10-08 22:34:55 +0530

    Added a sample music and also an endpoint to play the music

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req,res){
+	var fileId = req.query.id; 
+	var file = __dirname + '/../music/' + fileId;
+	console.log(file);
+	fs.exists(file,function(exists){
+		if(exists)
+		{
+			var rstream = fs.createReadStream(file);
+			rstream.pipe(res);
+		}
+		else
+		{
+			res.send("Its a 404");
+			res.end();
+		}
+
+	});
 });
 
 module.exports = router;
